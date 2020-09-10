178899f327b4ccaa709ad44ef9844319b8b2e1a6
davidsam1598 <66899390+davidsam1598@users.noreply.github.com>
2020-09-10 23:08:43 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+.style1 {
+	font-size: 36px;
+	font-weight: bold;
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	color: #FFFFFF;
+}
+.style3 {font-size: 18px}
+-->
+</style>
+</head>
+
+<body>
+<table width="100%" height="805" border="1">
+  <tr>
+    <td height="799" valign="top"><table width="100%" border="1">
+      <tr>
+        <td height="102" bgcolor="#33CC99"><p class="style1"><strong>BOOK  SHOPPING </strong></p>
+          <table width="70%" border="0" align="center">
+            <tr>
+              <td><div align="center"><strong><a href="index.php">Home</a></strong></div></td>
+              <td><div align="center"><strong><a href="admin.php">Admin</a></strong></div></td>
+              <td><div align="center"><strong><a href="login.php">User Login  </a></strong></div></td>
+              <td><div align="center"><strong><a href="register.php">Register</a></strong></div></td>
+              <td><div align="center"><strong><a href="#">About Us</a></strong></div></td>
+            </tr>
+          </table>          </td>
+      </tr>
+    </table>
+      <form id="form1" name="form1" method="post" action="">
+        <p>&nbsp;</p>
+        <table width="100%" border="0">
+          <tr>
+            <td colspan="2"><p align="left" class="style3"><strong>BOOK  SHOPPING APPLICATION:</strong></p>            </td>
+          </tr>
+          <tr>
+            <td width="28%" height="323"><img src="download.jpg" width="219" height="230" /></td>
+            <td width="72%" valign="middle"><div align="justify" class="style3">
+              <div align="justify">
+                <p>Bookstore application projects that acts as a central  database containing various books in stock along with their title, author and  cost. This project is a website that acts as a central book store. This web  project is developed using PHP as the front end and My SQL as a back-end. The My  SQL database stores various book related details. A user visiting the website  can see a wide range of books arranged in respective categories. The user may  select desired book and view its price. The user may even search for specific  books on the website. Once the user selects a book, user then has to fill in a  form and the book is booked for the user. Book Store Project is a Web Based book Shopping Project.  It is the graphical user interface. It has a form for user to input query  information to search the books from database. The control function is designed  to process the input from the users interface, generate the searching query and  then gets data from the database and returns to the users interface. User can  also buy and sell their books via in this application.</p>
+              </div>
+            </div></td>
+          </tr>
+        </table>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+      </form>      
+      <p>&nbsp;</p>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
